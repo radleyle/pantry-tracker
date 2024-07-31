80e436ea268244dcefe0f4602feef02c638360eb
Radley Le <radleyle1507@gmail.com>
2024-07-31 14:24:14 +0700

    latest update

---
+++ pantry_tracker/app/page.js
@@ -0,0 +1,8 @@
+'use client'
+import Image from "next/image";
+import {useState, useEffect} from 'react';
+import {firestore} from '@/firebase';
+
+export default function Home() {
+  return <>Hello World</>
+};
